44750715d0665a4b3e525f0240a062599b0eb04b
Super <supernitsuasnowpee@gmail.com>
2023-12-09 19:14:43 +1100

    updated to replit

--- RoleManagement/index.js
+++ RoleManagement/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField } = require("discord.js");
 
 function checkRoles(userRoles) {
-    return allowedRoles.some((allowedRole) => userRoles.has(allowedRole));
+  return allowedRoles.some((allowedRole) => userRoles.has(allowedRole));
 }
 
 const client = new Client({
@@ -14,93 +14,144 @@ const client = new Client({
   ],
 });
 
-client.on('ready', (c) => {
+client.on("ready", (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 
-  client.user.setUsername('Chief Scout James');
-  client.user.setAvatar('https://scouts.com.au/wp-content/uploads/2021/11/Scouts-Australia-Logo.jpg');
-  client.user.setStatus('dnd');
+  client.user.setUsername("Chief Scout James");
+  client.user.setAvatar(
+    "https://scouts.com.au/wp-content/uploads/2021/11/Scouts-Australia-Logo.jpg"
+  );
+  client.user.setStatus("dnd");
 });
 
 const allowedRoles = [
-    '1149950420039503943',
-    '1149950346534322217',
-    '1149950285385568318',
-    '1149950238870736926',
-    '1149950758163320862',
+  "1149950420039503943",
+  "1149950346534322217",
+  "1149950285385568318",
+  "1149950238870736926",
+  "1149950758163320862",
 ];
 
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
-  
-    const { commandName, options } = interaction;
-  
-    if (commandName === 'addcub') {
-      handleRoleUpdate(interaction, '1180323893857239131', '1149951272468885504');
-    }
-  
-    if (commandName === 'removeuser') {
-      handleRoleUpdate(interaction, '1179935988882096188');
-    }
-  
-    if (commandName === 'cubtoapl') {
-      handleRoleUpdate(interaction, '1180323893857239131 ', '1149951272468885504', '1180323913914384457', '1149951153166102599');
-    }
-  
-    if (commandName === 'cubtopl') {
-      handleRoleUpdate(interaction, '1180323893857239131 ', '1149951272468885504', '1180323913914384457', '1149951153166102599', '1149950799879884880');
-    }
-  
-    if (commandName === 'addscout') {
-      handleRoleUpdate(interaction, '1180323890401136682', '1149951272468885504');
-    }
-  
-    if (commandName === 'scouttoapl') {
-      handleRoleUpdate(interaction, '1180323890401136682', '1149951272468885504', '1180323912186331227', '1149951153166102599');
-    }
-  
-    if (commandName === 'scouttopl') {
-      handleRoleUpdate(interaction, '1180323890401136682', '1149951272468885504', '1180323912186331227', '1149951153166102599', '1149950799879884880');
-    }
-  
-    if (commandName === 'addventurer') {
-      handleRoleUpdate(interaction, '1180322973232668722', '1149951272468885504');
-    }
-  
-    if (commandName === 'venturertouc') {
-      handleRoleUpdate(interaction, '1180322973232668722', '1149951272468885504', '1180322948154933278', '1149951153166102599');
-    }
-  
-    if (commandName === 'venturertoaul') {
-      handleRoleUpdate(interaction, '1180322973232668722', '1149951272468885504', '1180322948154933278', '1149951153166102599', '1149950799879884880');
-    }
-  
-    if (commandName === 'venturertoul') {
-      handleRoleUpdate(interaction, '1180322973232668722', '1149951272468885504', '1180322948154933278', '1149951153166102599', '1149950799879884880', '1149950758163320862');
-    }
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName, options } = interaction;
+
+  if (commandName === "addcub") {
+    handleRoleUpdate(interaction, "1180323893857239131", "1149951272468885504");
+  }
+
+  if (commandName === "removeuser") {
+    handleRoleUpdate(interaction, "1179935988882096188");
+  }
+
+  if (commandName === "cubtoapl") {
+    handleRoleUpdate(
+      interaction,
+      "1180323893857239131 ",
+      "1149951272468885504",
+      "1180323913914384457",
+      "1149951153166102599"
+    );
+  }
+
+  if (commandName === "cubtopl") {
+    handleRoleUpdate(
+      interaction,
+      "1180323893857239131 ",
+      "1149951272468885504",
+      "1180323913914384457",
+      "1149951153166102599",
+      "1149950799879884880"
+    );
+  }
+
+  if (commandName === "addscout") {
+    handleRoleUpdate(interaction, "1180323890401136682", "1149951272468885504");
+  }
+
+  if (commandName === "scouttoapl") {
+    handleRoleUpdate(
+      interaction,
+      "1180323890401136682",
+      "1149951272468885504",
+      "1180323912186331227",
+      "1149951153166102599"
+    );
+  }
+
+  if (commandName === "scouttopl") {
+    handleRoleUpdate(
+      interaction,
+      "1180323890401136682",
+      "1149951272468885504",
+      "1180323912186331227",
+      "1149951153166102599",
+      "1149950799879884880"
+    );
+  }
+
+  if (commandName === "addventurer") {
+    handleRoleUpdate(interaction, "1180322973232668722", "1149951272468885504");
+  }
+
+  if (commandName === "venturertouc") {
+    handleRoleUpdate(
+      interaction,
+      "1180322973232668722",
+      "1149951272468885504",
+      "1180322948154933278",
+      "1149951153166102599"
+    );
+  }
+
+  if (commandName === "venturertoaul") {
+    handleRoleUpdate(
+      interaction,
+      "1180322973232668722",
+      "1149951272468885504",
+      "1180322948154933278",
+      "1149951153166102599",
+      "1149950799879884880"
+    );
+  }
+
+  if (commandName === "venturertoul") {
+    handleRoleUpdate(
+      interaction,
+      "1180322973232668722",
+      "1149951272468885504",
+      "1180322948154933278",
+      "1149951153166102599",
+      "1149950799879884880",
+      "1149950758163320862"
+    );
+  }
 });
 
 async function handleRoleUpdate(interaction, ...rolesToAdd) {
-    const member = interaction.member;
-    if (!checkRoles(member.roles.cache)) {
-      await interaction.reply('You do not have the required roles to use this command.');
-      return;
-    }
-  
-    const targetUser = interaction.options.getUser('user');
-    if (!targetUser) {
-      await interaction.reply('Please mention a user to update roles.');
-      return;
-    }
-  
-    const targetMember = interaction.guild.members.cache.get(targetUser.id);
-    if (!targetMember) {
-      await interaction.reply('User not found in the server.');
-      return;
-    }
-  
-    await targetMember.roles.set(rolesToAdd);
-    await interaction.reply('Roles updated successfully.');
-}  
+  const member = interaction.member;
+  if (!checkRoles(member.roles.cache)) {
+    await interaction.reply(
+      "You do not have the required roles to use this command."
+    );
+    return;
+  }
+
+  const targetUser = interaction.options.getUser("user"); // Corrected line
+  if (!targetUser) {
+    await interaction.reply("Please mention a user to update roles.");
+    return;
+  }
+
+  const targetMember = interaction.guild.members.cache.get(targetUser.id);
+  if (!targetMember) {
+    await interaction.reply("User not found in the server.");
+    return;
+  }
+
+  await targetMember.roles.set(rolesToAdd);
+  await interaction.reply("Roles updated successfully.");
+}
 
 client.login(process.env.TOKEN);
--- RoleManagement/register-commands.js
+++ RoleManagement/register-commands.js
@@ -1,146 +1,146 @@
 require('dotenv').config();
-const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
 
 const commands = [
-    {
-      name: 'addcub',
-      description: 'Gives the selected user the Basic Cub roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'removeuser',
-      description: 'Removes all roles from a user and gives them the Unknown role',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'cubtosapl',
-      description: 'Gives the selected user the Cub APL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'cubtopl',
-      description: 'Gives the selected user the Cub PL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'addscout',
-      description: 'Gives the selected user the Basic Scout roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'scouttoapl',
-      description: 'Gives the selected user the Scout APL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'scouttopl',
-      description: 'Gives the selected user the Scout PL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'addventurer',
-      description: 'Gives the selected user the Basic Venturer Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'venturertouc',
-      description: 'Gives the selected user the Venturer UC Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'venturertoaul',
-      description: 'Gives the selected user the Venturer AUL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: 'venturertoul',
-      description: 'Gives the selected user the Venturer UL Roles',
-      options: [
-        {
-          name: 'user',
-          description: 'Please select a user',
-          type: 6,
-          required: true,
-        },
-      ],
-    },
+  {
+    name: "addcub",
+    description: "Add Cub role to a user",
+    options: [
+      {
+        name: "user",
+        description: "The user to add the Cub role to",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "removeuser",
+    description: "Remove Cub role from a user",
+    options: [
+      {
+        name: "user",
+        description: "The user to remove the Cub role from",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "cubtoapl",
+    description: "Convert user from Cub to APL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Cub to APL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "cubtopl",
+    description: "Convert user from Cub to PL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Cub to PL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "addscout",
+    description: "Add Scout role to a user",
+    options: [
+      {
+        name: "user",
+        description: "The user to add the Scout role to",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "scouttoapl",
+    description: "Convert user from Scout to APL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Scout to APL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "scouttopl",
+    description: "Convert user from Scout to PL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Scout to PL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "addventurer",
+    description: "Add Venturer role to a user",
+    options: [
+      {
+        name: "user",
+        description: "The user to add the Venturer role to",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "venturertouc",
+    description: "Convert user from Venturer to UC",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Venturer to UC",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "venturertoaul",
+    description: "Convert user from Venturer to AUL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Venturer to AUL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "venturertoul",
+    description: "Convert user from Venturer to UL",
+    options: [
+      {
+        name: "user",
+        description: "The user to convert from Venturer to UL",
+        type: 6,
+        required: true,
+      },
+    ],
+  },
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Registering slash commands...');
+    console.log("Registering slash commands...");
 
     await rest.put(
       Routes.applicationGuildCommands(
@@ -150,7 +150,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
       { body: commands }
     );
 
-    console.log('Slash commands were registered successfully!');
+    console.log("Slash commands were registered successfully!");
   } catch (error) {
     console.log(`There was an error: ${error}`);
   }
